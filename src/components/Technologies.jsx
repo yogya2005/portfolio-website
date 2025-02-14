43dd03d6bd7dee5164a72667d73b3d9991a9831c
Yogya Agrawal <yogyanp@gmail.com>
2025-02-13 20:16:13 -0800

    Added Technologies component

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,24 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaNodeJs } from "react-icons/fa";
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <h1 className="my-20 text-center text-4xl">Technologies</h1>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <TbBrandNextjs className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaNodeJs className="text-7xl text-cyan-400" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Technologies;
