a38f549492820a023d075b4a18109ac1d5943130
Yogya Agrawal <yogyanp@gmail.com>
2025-02-13 19:17:08 -0800

    Added CSS for the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-  <nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
+  <nav className="mb-20 flex items-center justify-between py-6 pl-6">
+    <div className="left-paddingflex flex-shrink-0 items-center">
         <img src={logo} alt="Website Logo" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
