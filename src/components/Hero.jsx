a2abe482c51a07c6853258fe70079dc9629422b4
Yogya Agrawal <yogyanp@gmail.com>
2025-02-13 19:33:56 -0800

    Added CSS for the Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,16 @@
+import { HERO_CONTENT } from "../constants";
+
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
   <div className="flex flex-wrap">
     <div className="w-full lg:w-1/2">
       <div className="flex flex-col items-center lg:items-start">
-      <h1>Yogya Agrawal</h1>
-        <span>
+      <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Yogya Agrawal</h1>
+        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
             Student and Research Assistant
         </span>
-        <p>
+        <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
             {HERO_CONTENT}
         </p>
     </div>
