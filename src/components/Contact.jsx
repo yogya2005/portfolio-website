c18eaabc0994856b618e2c961945d351138064b2
Yogya Agrawal <yogyanp@gmail.com>
2025-02-13 21:20:41 -0800

    Added email hyperlink for testing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,8 @@ const Contact = () => {
       <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
       <div className="text-center tracking-tighter">
         <p className="my-4">{CONTACT.address}</p>
-        <a href="#" className="border-b">{CONTACT.email}</a>
+        <a href="mailto:${CONTACT.email}" className="border-b">{CONTACT.email}</a>
+        <a href="mailto:$yogya.agrawal2005@gmail.com" className="border-b">yogya.agrawal2005@gmail.com</a>
       </div>
     </div>
   );
