91f72aea7c884f6f9fc73c2a6d4cec6b6abb3a31
Yogya Agrawal <yogyanp@gmail.com>
2025-02-13 20:43:10 -0800

    Added Remaining Text for experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,6 +10,20 @@ const Experience = () => {
             <div className="w-full lg:w-1/4">
               <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
             </div>
+            <div className="w-full max-w-xl lg:w-3/4">
+            <h6 className="mb-2 font-semibold">
+                {experience.role} -{" "}
+                <span className="text-sm text-purple-100">
+                {experience.company}
+                </span>
+            </h6>
+            <p className="mb-4 text-neutral-400">{experience.description}</p>
+            {experience.technologies.map((tech, index) => (
+                <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">
+                {tech}
+                </span>
+            ))}
+            </div>
           </div>
         ))}
       </div>
