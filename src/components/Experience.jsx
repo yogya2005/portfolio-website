ae46e32623ac640b1af0f53fd071d0630aa665cd
Yogya Agrawal <yogyanp@gmail.com>
2025-02-13 20:28:02 -0800

    Edited Experience component to view initial changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,20 @@
+import { EXPERIENCES } from "../constants";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4xl">Experience</h1>
+      <div>
+        {EXPERIENCES.map((experience, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <div className="w-full lg:w-1/4">
+              <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
